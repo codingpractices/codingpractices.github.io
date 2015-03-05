2af61c6e085aecc122e3bff0ae2e479378de5cfc
Josh-D-L <zepieguy@gmail.com>
2015-03-04 21:21:45 -0600

    Update Code.js

--- Game/Code.js
+++ Game/Code.js
@@ -9,3 +9,22 @@ var imageRepository = new function() {
 	// Set images src
 	this.background.src = "Game/background.png";
 }
+/**
+ * Creates the Drawable object which will be the base class for
+ * all drawable objects in the game. Sets up default variables
+ * that all child objects will inherit, as well as the default
+ * functions.
+ */
+function Drawable() {
+	this.init = function(x, y) {
+		// Default variables
+		this.x = x;
+		this.y = y;
+	}
+	this.speed = 0;
+	this.canvasWidth = 0;
+	this.canvasHeight = 0;
+	// Define abstract function to be implemented in child objects
+	this.draw = function() {
+	};
+}
